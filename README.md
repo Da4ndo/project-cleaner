b3941ad984fbbffb9c17137274164c26eb7f74fc
Da4ndo <contact@da4ndo.com>
2024-01-27 22:35:25 +0100

    feat: Added READ.md and install.sh

---
+++ README.md
@@ -0,0 +1,49 @@
+![Forks](https://img.shields.io/github/forks/Da4ndo/projectcleaner?label=Forks&color=lime&logo=githubactions&logoColor=lime)
+![Stars](https://img.shields.io/github/stars/Da4ndo/projectcleaner?label=Stars&color=yellow&logo=reverbnation&logoColor=yellow)
+![License](https://img.shields.io/github/license/Da4ndo/projectcleaner?label=License&color=808080&logo=gitbook&logoColor=808080)
+![Issues](https://img.shields.io/github/issues/Da4ndo/projectcleaner?label=Issues&color=red&logo=ifixit&logoColor=red)
+
+# ProjectCleaner
+
+ProjectCleaner is a Rust utility designed to help programmers clean up build files and other unnecessary files from their projects. It offers a customizable and efficient methodology for specifying and deleting unwanted files and directories. Leveraging the power of Rust, it ensures a safe and fast cleaning process.
+Project made with ❤ by Da4ndo.
+
+You can click on the star (⭐️) button above this repository if you liked this project! Thank you all. 🙏
+
+## 🚀 Features
+
+- Customizable File and Directory Patterns
+- Safe Deletion of Unwanted Files
+- Supports Common Build Directories and File Types
+
+## 🛠️ Getting Started
+
+### 🔧 Installation
+
+You can install ProjectStructure by running the following command: 
+```bash
+git clone https://github.com/Da4ndo/project-cleaner
+cd project-cleaner
+
+cargo build
+
+chmod +x install.sh
+./install.sh
+```
+
+> The installation creates a folder `~/.da4ndo/`.
+
+## 📖 Documentation
+
+Project Cleaner is a highly efficient tool that leverages the power of Tokio's multi-threading to swiftly traverse through your project's directories, subdirectories, and files. 
+
+The application also takes exception folders and files, which you can use to specify files and directories that should be ignored during the deleting process. However, the presence of this is not mandatory, and the application will function normally without it.
+
+## 📝 License
+
+This project is licensed under the MIT License. See the **LICENSE** file for details.
+
+## 🤝 Contributing
+Contributions are welcome. Feel free to fix problems, report bugs, or propose new features. 
+
+
---
+++ install.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+pre_setup() {
+    # Create .da4ndo directory
+    if [ ! -d ~/.da4ndo ]; then
+        echo -ne "\033[0;35mCreating\033[0m ~/.da4ndo directory...\033[0m\r"
+        mkdir -p ~/.da4ndo
+        echo -e "\033[0;32mCreated\033[0m ~/.da4ndo directory.     \033[0m"
+    fi
+
+    # Check if env file is already there
+    if [ ! -f ~/.da4ndo/env ]; then
+        # Download env file
+        echo -ne "\033[0;35mDownloading\033[0m env file...\033[0m\r"
+        if ! wget https://cdn.da4ndo.com/env; then
+            if ! curl -O https://cdn.da4ndo.com/env; then
+                echo -e "\033[0;31mFailed to download the env file. Please check your internet connection and whether wget or curl is installed, then try again.\033[0m"
+                exit 1
+            fi
+        fi
+        echo -e "\033[0;32mDownloaded\033[0m env file.      \033[0m"
+
+        # Move the env file to the new directory
+        echo -ne "\033[0;35mMoving\033[0m env file to ~/.da4ndo/...\033[0m\r"
+        if ! mv env ~/.da4ndo/; then
+            echo -e "\033[0;31mFailed to move the env file to ~/.da4ndo/. Please check your permissions and try again.\033[0m"
+            exit 1
+        fi
+        echo -e "\033[0;32mMoved\033[0m env file to ~/.da4ndo/.    \033[0m"
+    fi
+
+    # Check if ~/.da4ndo/env is in PATH
+    if [[ ":$PATH:" != *":$HOME/.da4ndo/bin:"* ]] || { ! grep -q '. "$HOME/.da4ndo/env"' ~/.bashrc && ! grep -q '. "$HOME/.da4ndo/env"' ~/.zshrc; }; then
+        # Check if .bashrc exists
+        if [ -f ~/.bashrc ]; then
+            echo -ne "\033[0;35mAdding\033[0m '. "\$HOME/.da4ndo/env"' to ~/.bashrc...\033[0m\r"
+
+            echo '. "$HOME/.da4ndo/env"' >>~/.bashrc
+
+            echo -e "\033[0;32mAdded\033[0m '. "\$HOME/.da4ndo/env"' to ~/.bashrc.     \033[0m"
+        fi
+        # Check if .zshrc exists
+        if [ -f ~/.zshrc ]; then
+            echo -ne "\033[0;35mAdding\033[0m '. "\$HOME/.da4ndo/env"' to ~/.zshrc...\033[0m\r"
+
+            echo '. "$HOME/.da4ndo/env"' >>~/.zshrc
+
+            echo -e "\033[0;32mAdded\033[0m '. "\$HOME/.da4ndo/env" to ~/.zshrc.        \033[0m"
+        fi
+        echo -e "\033[0;32mAdded\033[0m \$HOME/.da4ndo/bin to PATH.     \033[0m"
+    fi
+}
+
+clean_previous_installations() {
+    # Check if project-cleaner is already installed
+    if [ -f ~/.da4ndo/bin/project-cleaner ]; then
+        echo -e "\033[0;35mRemoving previous installation of project-cleaner...\033[0m"
+        rm ~/.da4ndo/bin/project-cleaner -r
+        echo -e "\033[0;32mRemoved previous installation of project-cleaner.\033[0m"
+    fi
+}
+
+setup() {
+    # Check if binary is in the current folder or under target/debug or target/release
+    if [ -f ./project-cleaner ]; then
+        BINARY_PATH="./project-cleaner"
+    elif [ -f ./target/release/project-cleaner ]; then
+        BINARY_PATH="./target/release/project-cleaner"
+    elif [ -f ./target/debug/project-cleaner ]; then
+        BINARY_PATH="./target/debug/project-cleaner"
+    else
+        echo -e "\033[0;31mBinary not found. Please build the project and try again.\033[0m"
+        exit 1
+    fi
+
+    # Make the binary executable
+    echo -ne "\033[0;35mMaking\033[0m the project-cleaner binary executable...\033[0m\r"
+    if ! chmod +x $BINARY_PATH; then
+        echo -e "\033[0;31mFailed to make the project-cleaner binary executable. Please check your permissions and try again.\033[0m"
+        exit 1
+    fi
+    echo -e "\033[0;32mMade\033[0m the project-cleaner binary executable.     \033[0m"
+
+    # Check if project-cleaner is already installed
+    if
+        command -v project-cleaner >/dev/null
+    then
+        echo -e "\033[0;35mUpdating project-cleaner...\033[0m"
+        clean_previous_installations
+        if ! mv $BINARY_PATH ~/.da4ndo/bin/project-cleaner; then
+            echo -e "\033[0;31mFailed to update project-cleaner. Please check your permissions and try again.\033[0m"
+            exit 1
+        fi
+        echo -e "\033[0;32mUpdated project-cleaner.\033[0m"
+    else
+        echo -e "\033[0;35mproject-cleaner is not installed.\033[0m"
+        mkdir -p ~/.da4ndo/bin/
+        if ! mv $BINARY_PATH ~/.da4ndo/bin/project-cleaner; then
+            echo -e "\033[0;31mFailed to install project-cleaner. Please check your permissions and try again.\033[0m"
+            exit 1
+        fi
+        echo -e "\033[0;32mInstalled project-cleaner.\033[0m"
+    fi
+}
+
+pre_setup
+setup
+
+echo -e "\033[0;32mSetup completed successfully.\033[0m"
+if [[ ":$PATH:" != *":$HOME/.da4ndo/bin:"* ]]; then
+    echo -e "\033[0;35mPlease source the environment file or open a new terminal tab for the changes to take effect.\033[0m"
+    echo -e "\033[0;35m >\033[0m source \$HOME/.da4ndo/env \n"
+fi
